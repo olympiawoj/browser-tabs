87024f9dd46b3980b9f4f1439d064e8dce343782
olympiawoj <owojcik7@gmail.com>
2021-02-01 09:45:57 -0500

    React Router

--- src/App.js
+++ src/App.js
@@ -1,44 +1,40 @@
-import React, {useState} from 'react';
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route, NavLink} from 'react-router-dom'
+import Tab from './components/Tab'
+import Home from './pages/Home'
+import About from './pages/About'
+import Features from './pages/Features'
 import './App.css';
 
-function Tab ({children}){
-  const [highlightStyle, setHighlightStyle] = useState({left: 0, opacity: 0})
-
-  function moveHighlight(e){
-    console.log(e.nativeEvent)
-    // update highlightStyle to move the highlight 
-    setHighlightStyle({left: e.nativeEvent.layerX - 150})
-  }
-
-  function hideHighlight(e){
-    setHighlightStyle({
-      opacity: 0,
-      left: e.nativeEvent - 150,
-    })
-  }
-
-  return (
-    <div className="tab" onMouseOut={hideHighlight} onMouseMove={moveHighlight} >
-    <div className="highlight" style={highlightStyle}/>
-    {children}
-  </div>
-  )
-}
 
 function App() {
 
   return (
+    <Router>
     <div className="app">
       <div className="browser">
         <div className="tabs">
-            <Tab><a>Home</a></Tab>
-            <Tab><a>About</a></Tab>
-            <Tab><a>Features</a></Tab>
+            <Tab><NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink></Tab>
+            <Tab><NavLink to="/about" activeClassName="is-active">About</NavLink></Tab>
+            <Tab><NavLink to="/features" activeClassName="is-active">Features</NavLink></Tab>
         </div>
 
-        <div className="viewport">Pages Go Here</div>
+        <div className="viewport">
+            <Switch>
+            <Route path="/about">
+              <About/> 
+            </Route>
+            <Route path="/features">
+              <Features/> 
+            </Route>
+            <Route path="/" exact={true}>
+              <Home/> 
+            </Route>
+         </Switch>
+        </div>
       </div>
     </div>
+    </Router>
   );
 }
 
